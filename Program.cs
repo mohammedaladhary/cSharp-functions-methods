4f235d8973752140433f7f11ea83004335679fa4
mohammedaladhary <caladhary@gmail.com>
2023-06-06 11:18:40 +0400

    methods-functions

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        calculatefactorial(5);
+    }
+
+    static void calculatefactorial(int number)
+    {
+        int fact2 = 1;
+        for (int i = number; i > 1; i--)
+        {
+            fact2 = fact2 * i;
+        }
+        Console.WriteLine("The factorial of " + number + " is " + fact2);
+    }
+}
